9a25d00a0a534b243c0098a7db1656ba241f58dd
ekshustova <98463438+ekshustova@users.noreply.github.com>
2023-04-13 15:51:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
-from scipy import stats
+import scipy.stats as sps
 
-chat_id = 560481426 # Ваш chat ID, не меняйте название переменной
+chat_id = 897113152 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    otv = stats.ttest_1samp(x, 500).pvalue/2 > 0.05 or x.mean() > 500
-    return not otv
+def solution(x: np.array, y: np.array) -> bool: 
+    p_value = sps.permutation_test((x, y), lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis), 
+                 vectorized=True, 
+                 n_resamples=5000,
+                 alternative='greater').pvalue 
+    alpha = 0.07
+    return p_value < alpha
